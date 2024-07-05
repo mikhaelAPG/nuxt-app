413792485d27d33f7213327cc14b888fe79b28d3
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-07-06 00:30:33 +0700

    route parameter

---
+++ pages/products/[id].vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>Products detail for {{ id }}</p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, ducimus
+      quaerat laudantium officia aliquam quidem eligendi impedit! Laudantium,
+      voluptate maxime.
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+</script>
+
+<style scoped></style>
