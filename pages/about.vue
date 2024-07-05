8e0315ad38a4b5401a8d9c44f667a60a9f907863
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-07-06 00:25:07 +0700

    adding pages

---
+++ pages/about.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+      repellendus accusantium veritatis, quae suscipit quos voluptatum ipsa sint
+      optio sequi tempora in dolor expedita totam laudantium corporis ullam eos
+      deleniti?
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+
+p {
+  margin: 20px 0;
+}
+</style>
