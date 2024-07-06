cc39ac4f3469d57773bb9e7715937e7d6bf5fb83
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-07-06 11:00:23 +0700

    dynamic server routes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,10 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
-  devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
   app: {
     head: {
       title: "Nuxt Dojo",
-      meta: [{ name: "description", content: "Everything about nuxt 3" }],
+      meta: [{ name: "description", content: "Everything about Nuxt 3" }],
       link: [
         {
           rel: "stylesheet",
@@ -14,5 +12,8 @@ export default defineNuxtConfig({
         },
       ],
     },
+  },
+  runtimeConfig: {
+    currencyKey: process.env.CURRENCY_API_KEY,
   },
 });
--- pages/about.vue
+++ pages/about.vue
@@ -31,5 +31,7 @@ p {
 //   body: {age: 30}
 // })
 
-const { data } = await useFetch("/api/ninja");
+// const { data } = await useFetch('/api/ninja')
+
+const { data } = await useFetch("/api/currency/GBP");
 </script>
