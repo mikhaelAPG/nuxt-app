e789dc7ed824ae6c1f78adf5c9c58dc27dcc8c19
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-07-06 09:50:08 +0700

    Fetching Data

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,16 +1,17 @@
 <template>
   <div>
-    <p>Products detail for {{ id }}</p>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, ducimus
-      quaerat laudantium officia aliquam quidem eligendi impedit! Laudantium,
-      voluptate maxime.
-    </p>
+    <p>{{ product.title }}</p>
+    <p>{{ product.price }}</p>
+    <p>{{ product.id }}</p>
   </div>
 </template>
 
 <script setup>
 const { id } = useRoute().params;
+const uri = "https://fakestoreapi.com/products/" + id;
+
+// fetch the product
+const { data: product } = await useFetch(uri, { key: id });
 
 definePageMeta({
   layout: "products",
